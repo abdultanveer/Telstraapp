9100c306e9ba1e34a3019920f594e075466e843f
abdul <abdul.tanveer@gmail.com>
2021-09-06 14:50:06 +0530

    handle click of the button gettext from edittext and set it on textview

--- app/src/main/java/com/abdul/telstraapp/MainActivity.kt
+++ app/src/main/java/com/abdul/telstraapp/MainActivity.kt
@@ -3,19 +3,25 @@ package com.abdul.telstraapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     lateinit var person:Person
+    lateinit var nameEt: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         person = Person("abdul",321)
-
+        nameEt = findViewById(R.id.etName)
     }
 
     fun clickHandler(view: View) {
+        var name = nameEt.text
+        var tvResult: TextView = findViewById(R.id.tvResult)
+        tvResult.setText(name)
         Toast.makeText(this,"welcome to android",Toast.LENGTH_SHORT).show()
     }
 }
