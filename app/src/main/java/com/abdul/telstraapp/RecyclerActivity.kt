4d9260e7c67d73ff681d51e693b1409b801e2af3
abdul <abdul.tanveer@gmail.com>
2021-09-08 11:20:10 +0530

    recyclerview steps

--- app/src/main/java/com/abdul/telstraapp/RecyclerActivity.kt
+++ app/src/main/java/com/abdul/telstraapp/RecyclerActivity.kt
@@ -6,14 +6,14 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class RecyclerActivity : AppCompatActivity() {
-    var wordsData = arrayOf("first","second","third","fourth")
+    var wordsData = arrayOf("first","second","third","fourth")// step 3
     lateinit var wordsAdapter: WordsAdapter
     lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycler)
-        wordsAdapter = WordsAdapter(wordsData)
+        wordsAdapter = WordsAdapter(wordsData) // step 10
         recyclerView = findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = wordsAdapter
--- app/src/main/java/com/abdul/telstraapp/WordsAdapter.kt
+++ app/src/main/java/com/abdul/telstraapp/WordsAdapter.kt
@@ -6,16 +6,16 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class WordsAdapter(val wordsData: Array<String>) : RecyclerView.Adapter<WordsAdapter.WordsViewHolder>() {
+class WordsAdapter(val wordsData: Array<String>) : RecyclerView.Adapter<WordsAdapter.WordsViewHolder>() { //step 4
 
-    class WordsViewHolder(rowView: View) :RecyclerView.ViewHolder(rowView){
-        val rowTextView: TextView = rowView.findViewById(R.id.tvRow)
+    class WordsViewHolder(rowView: View) :RecyclerView.ViewHolder(rowView){//step 5
+        val rowTextView: TextView = rowView.findViewById(R.id.tvRow) //step 8
     }
 
     /**
      * mayur -- he has buy/create new planks for rows --
      */
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordsViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordsViewHolder { //step 6
         var createdRow = LayoutInflater.from(parent.context).inflate(R.layout.row_rv,parent,false)
         return WordsViewHolder(createdRow)
     }
@@ -23,14 +23,14 @@ class WordsAdapter(val wordsData: Array<String>) : RecyclerView.Adapter<WordsAda
     /**
      * mohit -- handwriting is good, he'll write on the plank
      */
-    override fun onBindViewHolder(holder: WordsViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: WordsViewHolder, position: Int) { //step 7
         holder.rowTextView.text = wordsData[position]
     }
 
     /**
      * batula: keep the count of data items
      */
-    override fun getItemCount(): Int {
+    override fun getItemCount(): Int { //step 9
         return wordsData.size
     }
 }
