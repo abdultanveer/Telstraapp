c684b8ebdd8f55121b10be896518010ef1c4a0c2
abdul <abdul.tanveer@gmail.com>
2021-09-13 15:55:46 +0530

    get data from firestore

--- app/src/main/java/com/abdul/telstraapp/FirebaseActivity.java
+++ app/src/main/java/com/abdul/telstraapp/FirebaseActivity.java
@@ -4,15 +4,20 @@ package com.abdul.telstraapp;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,11 +25,13 @@ import java.util.Map;
 public class FirebaseActivity extends AppCompatActivity {
     public static String TAG = FirebaseActivity.class.getSimpleName();
     FirebaseFirestore db;
+    TextView firestoreText;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_firebase);
          db = FirebaseFirestore.getInstance();
+         firestoreText = findViewById(R.id.tvFirestore);
     }
 
     public void sendData(View view) {
@@ -46,6 +53,23 @@ public class FirebaseActivity extends AppCompatActivity {
                     @Override
                     public void onFailure(@NonNull Exception e) {
                         Log.w(TAG, "Error adding document", e);
+                    }
+                });
+    }
+
+    public void getData(View view) {
+        db.collection("users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
                     }
                 });
     }
